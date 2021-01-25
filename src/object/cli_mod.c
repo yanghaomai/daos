23e880a6abd25cb2a80c6d98fb78ce63a1b4f78c
Liang Zhen <liang.zhen@intel.com>
2021-01-25 15:25:52 +0800

    DAOS-5298 cleanup and fix for EC aggregation
    - Add inline functions to ease access to structure members
    - remove debug code from do_obj_ioc_begin
    - reduce code indentation of a few functions
    
    Fixes:
    - NLT fix for dc_obj_init
    - bug fix for the loop in agg_get_obj_handle
    
    Signed-off-by: Liang Zhen <liang.zhen@intel.com>

--- src/object/cli_mod.c
+++ src/object/cli_mod.c
@@ -54,25 +54,32 @@ dc_obj_init(void)
 
 	rc = obj_utils_init();
 	if (rc)
-		goto out;
+		return rc;
 
 	rc = obj_class_init();
 	if (rc)
-		goto out;
+		goto out_utils;
 
 	rc = daos_rpc_register(&obj_proto_fmt, OBJ_PROTO_CLI_COUNT,
 				NULL, DAOS_OBJ_MODULE);
 	if (rc != 0) {
 		D_ERROR("failed to register daos obj RPCs: "DF_RC"\n",
 			DP_RC(rc));
-		D_GOTO(out, rc);
+		D_GOTO(out_oc, rc);
 	}
 
 	rc = obj_ec_codec_init();
-	if (rc != 0)
+	if (rc != 0) {
 		D_ERROR("failed to obj_ec_codec_init: "DF_RC"\n", DP_RC(rc));
-
-out:
+		goto out_rpc;
+	}
+	return 0;
+out_rpc:
+	daos_rpc_unregister(&obj_proto_fmt);
+out_oc:
+	obj_class_fini();
+out_utils:
+	obj_utils_fini();
 	return rc;
 }
 
